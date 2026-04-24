1dcec93c278bd103404665d08c6f4dd905446e35
Kerilous <kyrosamy1@gmail.com>
2026-04-24 21:26:26 +0300

    Handled equal numbers edge case in maxNumber function

--- Lib_Folder/Task7.dart
+++ Lib_Folder/Task7.dart
@@ -2,9 +2,9 @@
 import "dart:io";
 double maxNumber(double num1,double num2,double num3)
 {
-  if(num1>num2&&num1>num3)
+  if(num1>=num2&&num1>=num3)
   return num1;
-  else if(num2>num1&&num2>num3)
+  else if(num2>=num1&&num2>=num3)
   return num2;
   else return num3;
 
